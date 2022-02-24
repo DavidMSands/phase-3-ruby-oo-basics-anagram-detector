00b1f3f2d40e00b026b7ac3689f87cc873dc5938
David <Davidmaxsands@gmail.com>
2022-02-23 18:21:19 -0700

    commit tree 781da8568c7d8793fbaebac370e55576209056c0

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
-# Your code goes here!
+class Anagram 
+    def initialize(word)
+        @word = word
+    end
+
+    def match arry
+      new_arry =  arry.filter do |ech|
+        @word.chars.sort == ech.chars.sort
+        end
+        new_arry
+    end
+end 
+
+bored = Anagram.new('bored')
+puts bored.match(%w[fish friends robed])
+
